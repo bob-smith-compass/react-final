40075025fdef84df07b21a9624a883a97f1e1f9e
David Shams <david.shams@gmail.com>
2019-02-23 02:32:19 -0600

    createa List component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react';
 // import logo from './logo.svg';
 import './App.css';
+import List from './components/list/List';
 
 class App extends Component {
   render() {
     return (
-      <div>React</div>
+      <div>
+        <List />
+      </div>
+
     );
   }
 }

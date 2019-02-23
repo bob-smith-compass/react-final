cdfddf6aefe036d36cebbb85220af7ff7c6a6efa
David Shams <david.shams@gmail.com>
2019-02-23 03:32:26 -0600

    key={index of array} Bootstrap Card returned from dynamic elements

--- src/App.js
+++ src/App.js
@@ -7,8 +7,17 @@ class App extends Component {
   render() {
     return (
       <div>
-        {this.props.names.map((e) => {
-          return <div>{e}</div>
+        {this.props.names.map((e, i) => {
+          return <div key={i}>
+            <div className="card" style={{width: "18rem"}}>
+              {/* <img className="card-img-top" src=".../100px180/" alt="Card image cap" /> */}
+              <div className="card-body">
+                <h5 className="card-title">{e}</h5>
+                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                <a href="#" className="btn btn-primary">See {i}. {e} </a>
+              </div>
+            </div>
+          </div>
         })}
         <List />
       </div>
@@ -17,19 +26,3 @@ class App extends Component {
 }
 
 export default App;
-
-
-
-        // {/* {this.props.names.map((e) => {
-        //   return (
-
-        //     <div>
-        //       <div className="card" style="width: 18rem;">
-        //         {/* <img className="card-img-top" src=".../100px180/" alt="Card image cap" /> */}
-        //         <div className="card-body">
-        //         <h5 className="card-title">Card title</h5>
-        //         <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-        //         <a href="#" className="btn btn-primary">Go somewhere</a>
-        //       </div>
-        // </div>
-        //   </div> )} */}

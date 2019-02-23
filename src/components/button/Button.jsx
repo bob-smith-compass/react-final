5f5ed93938835549ee6bc38ba96cf44bbcc5aa3a
David Shams <david.shams@gmail.com>
2019-02-23 07:29:57 -0600

    this.handleClick = this.handleClick.bind(this);

--- src/components/button/Button.jsx
+++ src/components/button/Button.jsx
@@ -3,7 +3,8 @@ import React, { Component } from 'react';
 export default class Button extends Component {
     constructor(props) {
         super(props);
-        this.state = {clicks: 0}
+        this.state = {clicks: 0};
+        this.handleClick = this.handleClick.bind(this);
     }
     handleClick() {
         this.setState({clicks: this.state.clicks + 1})

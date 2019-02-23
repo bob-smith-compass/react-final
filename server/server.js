e28cce28ced4fada8eb666fdb4eee3e0d6d58c6c
David Shams <david.shams@gmail.com>
2019-02-23 05:17:33 -0600

    yarn add chalk

--- server/server.js
+++ server/server.js
@@ -1,7 +1,11 @@
 const express = require('express')
+const path = require('path');
 const app = express()
 const port = 9090
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/', (req, res) => {
+    // res.send('Hello World!')
+    res.render(path.join(__dirname, 'public/index'));
+})
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))

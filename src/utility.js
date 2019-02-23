fe448fb21b07e752a5f0f7294f46a2c889134b12
David Shams <david.shams@gmail.com>
2019-02-23 04:17:40 -0600

    utility.js reduce and map

--- src/utility.js
+++ src/utility.js
@@ -1,3 +1,6 @@
+/**
+ * promise
+ */
 var promise1 = new Promise(function(resolve, reject) {
     setTimeout(function() {
       resolve('foo');
@@ -11,3 +14,28 @@ var promise1 = new Promise(function(resolve, reject) {
   
   console.log(promise1);
   // expected output: [object Promise]
+
+/**
+ * reduce
+ */  
+const array1 = [1, 2, 3, 4];
+const reducer = (accumulator, currentValue) => accumulator + currentValue;
+
+// 1 + 2 + 3 + 4
+console.log(array1.reduce(reducer));
+// expected output: 10
+
+// 5 + 1 + 2 + 3 + 4
+console.log(array1.reduce(reducer, 5));
+// expected output: 15
+
+/**
+ * map
+ */
+var array1 = [1, 4, 9, 16];
+
+// pass a function to map
+const map1 = array1.map(x => x * 2);
+
+console.log(map1);
+// expected output: Array [2, 8, 18, 32]

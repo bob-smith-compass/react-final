2e785fefdc771aac291d2b836a1cc102ec785b6f
David Shams <david.shams@gmail.com>
2019-02-23 05:21:25 -0600

    chalk.green()

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const path = require('path');
+const chalk = require('chalk');
 const app = express()
 const port = 9090
 
@@ -8,4 +9,4 @@ app.get('/', (req, res) => {
     res.render(path.join(__dirname, 'public/index'));
 })
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.listen(port, () => console.log(`Example app listening on port ${chalk.green(port)}!`))

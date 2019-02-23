d090eb413fc37e2a8343318b98aae680f940c361
David Shams <david.shams@gmail.com>
2019-02-23 05:25:14 -0600

    DEBUG=* node server.js

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const path = require('path');
 const chalk = require('chalk');
+const debug = require('debug');
 const app = express()
 const port = 9090
 
@@ -9,4 +10,10 @@ app.get('/', (req, res) => {
     res.render(path.join(__dirname, 'public/index'));
 })
 
-app.listen(port, () => console.log(`Example app listening on port ${chalk.green(port)}!`))
+app.listen(port, () => {
+    // console.log(`Example app listening on port ${chalk.green(port)}!`)
+    /**
+     * DEBUG=* node server.js 
+     */
+    debug(`Example app listening on port ${chalk.green(port)}!`); 
+})

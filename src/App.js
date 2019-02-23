d40ed97fcffbf6ce6d6e72a85bb81c07011ac27e
David Shams <david.shams@gmail.com>
2019-02-23 03:39:04 -0600

    event handling onClick

--- src/App.js
+++ src/App.js
@@ -4,6 +4,9 @@ import './App.css';
 import List from './components/list/List';
 
 class App extends Component {
+  handleClick() {
+    alert('Clicked');
+  }
   render() {
     return (
       <div>
@@ -14,7 +17,7 @@ class App extends Component {
               <div className="card-body">
                 <h5 className="card-title">{e}</h5>
                 <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                <a href="#" className="btn btn-primary">See {i}. {e} </a>
+                <a href="#" onClick={this.handleClick} className="btn btn-primary">See {i}. {e} </a>
               </div>
             </div>
           </div>
